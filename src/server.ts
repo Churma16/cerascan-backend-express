38875ec4de9fd14bf78b952fd3ab0403b83e6ba8
Fathan <74785057+Churma16@users.noreply.github.com>
2026-05-10 21:50:06 +0700

    feat: implement database connection and server startup logic

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,28 @@
 import app from './app';
+import sequelize from './config/database';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`🚀 API Gateway berjalan di http://localhost:${PORT}`);
-});
+const startServer = async () => {
+    try {
+        // 1. Tes koneksi database
+        await sequelize.authenticate();
+        console.log('✅ Koneksi ke MySQL berhasil.');
+
+        // 2. Sinkronisasi tabel (Jika tabel belum ada, Sequelize akan otomatis membuatnya!)
+        await sequelize.sync({alter: true});
+        console.log('✅ Semua model telah disinkronisasi dengan database.');
+
+        // 3. Nyalakan server
+        app.listen(PORT, () => {
+            console.log(`🚀 API Gateway berjalan di http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error('❌ Tidak dapat terhubung ke database:', error);
+    }
+};
+
+startServer();
