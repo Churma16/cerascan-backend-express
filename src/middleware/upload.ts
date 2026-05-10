c0d28a090f598efa6a41732f46ca54d7779df820
Fathan <74785057+Churma16@users.noreply.github.com>
2026-05-11 04:07:20 +0700

    feat: add upload middleware for handling file uploads

---
+++ src/middleware/upload.ts
@@ -0,0 +1,20 @@
+import * as fs from "node:fs";
+import path from "node:path";
+import multer from "multer";
+
+const uploadDir = path.join(__dirname, '../../uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, {recursive: true});
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + '-' + file.originalname);
+    }
+});
+
+export const uploadMiddleware = multer({storage: storage});
