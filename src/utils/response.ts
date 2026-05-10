a6a498a39031f715cdbd7af5d5fa06930da57320
Fathan <74785057+Churma16@users.noreply.github.com>
2026-05-11 04:05:48 +0700

    feat: add sendResponseMulti function for multi-item responses

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -8,3 +8,17 @@ export const sendResponse = (res: any, code: number, message: string, data: any 
         data: data
     });
 };
+
+export const sendResponseMulti = (res: any, code: number, message: string, data: any = null) => {
+    return res.status(code).json({
+        meta: {
+            code: code,
+            status: code < 400 ? 'success' : 'error',
+            message: message,
+            count: data ? data.length : 0,
+        },
+        data: data
+    });
+};
+
+
