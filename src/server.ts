263d00cb06cde754d5ca8da066c7816265dae12a
Fathan <74785057+Churma16@users.noreply.github.com>
2026-05-11 04:36:48 +0700

    refactor: improve comments and sync method in server.ts

--- src/server.ts
+++ src/server.ts
@@ -11,15 +11,15 @@ const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
     try {
-        // 1. Tes koneksi database
+        // Check Connection
         await sequelize.authenticate();
-        console.log('✅ Koneksi ke MySQL berhasil.');
+        console.log('Koneksi ke MySQL berhasil.');
 
-        // 2. Sinkronisasi tabel (Jika tabel belum ada, Sequelize akan otomatis membuatnya!)
-        await sequelize.sync({alter: true});
+        // Sync Table
+        await sequelize.sync();
         console.log('✅ Semua model telah disinkronisasi dengan database.');
 
-        // 3. Nyalakan server
+        // Start Server
         app.listen(PORT, () => {
             console.log(`🚀 API Gateway berjalan di http://localhost:${PORT}`);
         });
