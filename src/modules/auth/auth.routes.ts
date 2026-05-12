4c1b4c5b76d23913fd9fe5354034d96fb376c13b
Fathan <74785057+Churma16@users.noreply.github.com>
2026-05-13 00:22:42 +0700

    feat: add authentication middleware to routes for enhanced security

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,7 +1,9 @@
 import {Router} from 'express';
 import {AuthController} from './auth.controller';
+import {requireAuth} from "../../middleware/auth.guard";
 
 const router = Router();
+// router.use(requireAuth);
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
