9f5eccfefea0e06bb71907cdfcddcf234fda8e69
Fathan <74785057+Churma16@users.noreply.github.com>
2026-05-21 19:51:21 +0700

    feat: add rate limiter to forgot password route

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 import {AuthController} from './auth.controller';
 import {requireAuth} from "../../middleware/auth.guard";
+import {forgotPasswordLimiter} from "../../middleware/rate_limit";
 
 const router = Router();
 // router.use(requireAuth);
@@ -8,7 +9,7 @@ const router = Router();
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
 router.post('/change-password', requireAuth, AuthController.changePassword);
-router.post('/forgot-password', AuthController.forgotPassword);
+router.post('/forgot-password', forgotPasswordLimiter, AuthController.forgotPassword);
 router.post('/verify-otp', AuthController.verifyOtp);
 router.post('/reset-password', AuthController.resetPassword);
 export default router;
