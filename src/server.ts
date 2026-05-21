259bff94a9afbd45736246ef139d138a9278bc2b
Fathan <74785057+Churma16@users.noreply.github.com>
2026-05-21 19:42:23 +0700

    fix: clean up console log messages in server initialization

--- src/server.ts
+++ src/server.ts
@@ -17,14 +17,15 @@ const startServer = async () => {
 
         // Sync Table
         await sequelize.sync();
-        console.log('✅ Semua model telah disinkronisasi dengan database.');
+        // await sequelize.sync({alter: true});
+        console.log(' Semua model telah disinkronisasi dengan database.');
 
         // Start Server
         app.listen(PORT, () => {
-            console.log(`🚀 API Gateway berjalan di http://localhost:${PORT}`);
+            console.log(`API Gateway berjalan di http://localhost:${PORT}`);
         });
     } catch (error) {
-        console.error('❌ Tidak dapat terhubung ke database:', error);
+        console.error('Tidak dapat terhubung ke database:', error);
     }
 };
 
