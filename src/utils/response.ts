b5ee82df3689ec1b1771d548244aa130592d376d
Fathan <74785057+Churma16@users.noreply.github.com>
2026-05-22 16:42:01 +0700

    feat(response): add source parameter to sendResponse and sendResponseMulti functions

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,19 +1,21 @@
-export const sendResponse = (res: any, code: number, message: string, data: any = null) => {
+export const sendResponse = (res: any, code: number, message: string, data: any = null, source: string = 'db') => {
     return res.status(code).json({
         meta: {
             code: code,
             status: code < 400 ? 'success' : 'error',
+            source: source,
             message: message,
         },
         data: data
     });
 };
 
-export const sendResponseMulti = (res: any, code: number, message: string, data: any = null) => {
+export const sendResponseMulti = (res: any, code: number, message: string, data: any = null, source: string = 'db') => {
     return res.status(code).json({
         meta: {
             code: code,
             status: code < 400 ? 'success' : 'error',
+            source: source,
             message: message,
             count: data ? data.length : 0,
         },
