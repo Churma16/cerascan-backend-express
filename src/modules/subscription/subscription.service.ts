1cc2bf1b070006c33d584c64d04de569541439a1
Fathan <74785057+Churma16@users.noreply.github.com>
2026-06-01 17:41:15 +0700

    refactor(subscription): rename downgradeSubscription to expireActiveSubscriptions and streamline subscription expiration logic

--- src/modules/subscription/subscription.service.ts
+++ src/modules/subscription/subscription.service.ts
@@ -1,7 +1,6 @@
-import {Plan, Subscription, User} from "../../models";
+import {Plan, Subscription} from "../../models";
 import {PlanService} from "../plan/plan.service";
-import {Op, Sequelize, Transaction} from "sequelize";
-import {getNowIndonesiaTime} from "../../utils/time.helper";
+import {Op, Transaction} from "sequelize";
 
 export interface SubscriptionPayload {
     user_id: number;
@@ -106,43 +105,17 @@ export class SubscriptionService {
         return subsDetail?.toJSON();
     }
 
-    static async downgradeSubscription(t?: Transaction) {
-        const today = getNowIndonesiaTime();
-        const expiredSubsSubquery = Sequelize.literal(`(
-        SELECT user_id 
-        FROM "Subscriptions" 
-        WHERE status = 'active' AND end_date <= '${today}' )`);
-
-        const [updatedUsersCount] = await User.update(
-            {
-                plan_id: 1
-            },
-            {
-                where: {
-                    id: {
-                        [Op.in]: expiredSubsSubquery
-                    }
-                },
-                transaction: t
-            }
-        );
-
-        const [updatedSubsCount] = await Subscription.update(
+    static async expireActiveSubscriptions(today: Date, t?: Transaction) {
+        const [affectedCount] = await Subscription.update(
             {status: 'expired'},
             {
                 where: {
-                    end_date: {
-                        [Op.lte]: today
-                    },
+                    end_date: {[Op.lte]: today},
                     status: 'active'
                 },
                 transaction: t
             }
         );
-
-        console.log(`[Cron Log] User di-downgrade: ${updatedUsersCount}, Subs di-expired: ${updatedSubsCount}`);
-        return {updatedUsersCount, updatedSubsCount};
+        return affectedCount;
     }
-
-
 }
