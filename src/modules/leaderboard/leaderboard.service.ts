f0d865515577590f16fee32a7cfc53bca0c6c623
Fathan <74785057+Churma16@users.noreply.github.com>
2026-06-01 16:53:58 +0700

    feat(leaderboard): implement syncLeaderboardDataToDb method for MySQL data synchronization

--- src/modules/leaderboard/leaderboard.service.ts
+++ src/modules/leaderboard/leaderboard.service.ts
@@ -1,6 +1,6 @@
 import {getRedisClient} from "../../config/redis_client";
 import {getCurrentMonthIdIndonesia} from "../../utils/time.helper";
-import {User} from "../../models";
+import {LeaderboardArchive, User} from "../../models";
 
 export class LeaderboardService {
     /**
@@ -94,6 +94,53 @@ export class LeaderboardService {
             console.error(error);
             throw error;
         }
+    }
+
+    static async syncLeaderboardDataToDb(redis: any) {
+        const currentPeriod = getCurrentMonthIdIndonesia();
+        const redisRankKey = `leaderboard:rank:${currentPeriod}`;
+
+        const activeUserIds = await redis.sendCommand(['ZREVRANGE', redisRankKey, '0', '-1']) as string[];
+
+        const noActiveScannerThisMonth = !activeUserIds || activeUserIds.length === 0;
+        if (noActiveScannerThisMonth) {
+            console.log('✅ [Cron] Tugas 3 dilewati. Tidak ada data leaderboard bulan ini.');
+            return;
+        }
+
+        const statsPipeline = redis.multi();
+
+        for (const userIdString of activeUserIds) {
+            const userStatsKey = `leaderboard:stats:${currentPeriod}:user:${userIdString}`;
+            statsPipeline.hGetAll(userStatsKey);
+        }
+
+        const rawStatsResults = await statsPipeline.exec();
+
+        const recordsToUpsert: any[] = [];
+
+        activeUserIds.forEach((userIdString, index) => {
+            const userId = parseInt(userIdString);
+
+            const userMetrics = rawStatsResults[index] as Record<string, string>;
+
+            if (userMetrics) {
+                recordsToUpsert.push({
+                    user_id: userId,
+                    period: currentPeriod,
+                    total_scans: parseInt(userMetrics.total_scans || '0'),
+                    defect_scans: parseInt(userMetrics.defect_scans || '0')
+                });
+            }
+        });
+
+        if (recordsToUpsert.length > 0) {
+            await LeaderboardArchive.bulkCreate(recordsToUpsert, {
+                updateOnDuplicate: ['total_scans', 'defect_scans']
+            });
+        }
+
+        console.log(`✅ [Cron] Tugas 3 Selesai. ${recordsToUpsert.length} rekor leaderboard diamankan ke MySQL.`);
     }
 
 }
