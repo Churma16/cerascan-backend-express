647b36ca539c0bc17088790f3a9e5b093fa70657
Fathan <74785057+Churma16@users.noreply.github.com>
2026-06-30 23:05:57 +0700

    feat: initiate grpc instance

---
+++ src/config/grpc_client.ts
@@ -0,0 +1,24 @@
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
+import * as path from 'path';
+
+const PROTO_PATH = path.resolve(__dirname, '../../proto/ceramic.proto');
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+});
+
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition) as any;
+const ceramicProto = protoDescriptor.ceramic;
+
+const GRPC_SERVER_URL = process.env.PYTHON_ML_GRPC_URL || 'localhost:50051';
+
+// initiate grpc instance
+export const grpcCeramicClient = new ceramicProto.CeramicService(
+    GRPC_SERVER_URL,
+    grpc.credentials.createInsecure()
+);
